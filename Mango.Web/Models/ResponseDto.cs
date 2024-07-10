aec1353d88af912d6b14815bbc41b1ea0d8b1937
Moise Rajesearison <moise.rajesearison@mvola.mg>
2024-07-10 16:44:43 +0300

    first commit

---
+++ Mango.Web/Models/ResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Mango.Web.Models
+{
+    public class ResponseDto
+    {
+        public object? Result { get; set; }
+        public bool isSuccess { get; set; } = true;
+        public string Message  { get; set; } = string.Empty;
+    }
+}
---
+++ Mango.Web/Service/IService/ICouponService.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface ICouponService
+    {
+        Task<ResponseDto?> GetCouponAsync(string couponCode);
+        Task<ResponseDto?> GetAllCouponAsync();
+        Task<ResponseDto?> GetCouponByIdAsync(int id);
+        Task<ResponseDto?> CreateCouponsAsync(CouponDto couponDto);
+        Task<ResponseDto?> UpdateCouponsAsync(CouponDto couponDto);
+        Task<ResponseDto?> DeleteCouponsAsync(int id);
+
+    }
+}
